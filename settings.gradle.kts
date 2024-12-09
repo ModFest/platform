65f788566ce840d8842f7c6ed9b1053cb82d078f
TheEpicBlock <git.teb@theepicblock.nl>
2024-12-09 12:25:11 +0100

    Add kordex bot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		mavenCentral()
+
+		maven("https://snapshots-repo.kordex.dev")
+		maven("https://releases-repo.kordex.dev")
+	}
+}
+
 rootProject.name = "platform"
 include("platform_api")
