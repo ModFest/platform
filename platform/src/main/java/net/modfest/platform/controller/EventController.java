e0a75c19e95cc39db9db0a0b54a42da991a31008
TheEpicBlock <git.teb@theepicblock.nl>
2024-12-09 01:52:44 +0100

    Proper 404

--- platform/src/main/java/net/modfest/platform/controller/EventController.java
+++ platform/src/main/java/net/modfest/platform/controller/EventController.java
@@ -3,11 +3,13 @@ package net.modfest.platform.controller;
 import net.modfest.platform.pojo.EventData;
 import net.modfest.platform.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/event", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -17,6 +19,10 @@ public class EventController {
 
 	@GetMapping("/{id}")
 	public EventData getEvent(@PathVariable String id) {
-		return repository.get(id);
+		var event = repository.get(id);
+		if (event == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such event exists");
+		}
+		return event;
 	}
 }
