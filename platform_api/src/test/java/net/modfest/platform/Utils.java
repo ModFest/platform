8f349a55d69ef45dc8faec60c8b59ccabb8120d8
TheEpicBlock <git.teb@theepicblock.nl>
2025-02-22 20:51:31 +0100

    Fix test
    Error introduced in 4397be08934e09aefedb819075841f0e8cbde270

--- platform_api/src/test/java/net/modfest/platform/Utils.java
+++ platform_api/src/test/java/net/modfest/platform/Utils.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 public class Utils {
 	public static ManagedDirectory getTestDir(Path dir) {
 		try {
-			return new GitRootPath(dir, new GitConfig("debug", "debug@example.org"));
+			return new GitRootPath(dir, new GitConfig("debug", "debug@example.org", null, null));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
