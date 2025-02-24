74c9467b11a2292449fdc62349f1a1de7e9bce69
TheEpicBlock <git.teb@theepicblock.nl>
2025-02-24 14:30:15 +0100

    Fix requests getting messed up during git errors

--- platform_api/src/main/java/net/modfest/platform/git/GitRequestInterceptor.java
+++ platform_api/src/main/java/net/modfest/platform/git/GitRequestInterceptor.java
@@ -5,10 +5,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import net.modfest.platform.pojo.UserData;
 import net.modfest.platform.security.BotFestIdentity;
 import org.apache.shiro.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class GitRequestInterceptor implements AsyncHandlerInterceptor {
+	private static final Logger LOGGER = LoggerFactory.getLogger(GitRequestInterceptor.class);
 	private final GlobalGitManager git;
 
 	public GitRequestInterceptor(GlobalGitManager git) {
@@ -35,13 +38,22 @@ public class GitRequestInterceptor implements AsyncHandlerInterceptor {
 		return true;
 	}
 
+	private void onClose() {
+		try {
+			git.closeScope();
+		} catch (Exception e) {
+			// We can't throw exceptions at this point, it'll mess up the response
+			LOGGER.error("Error during finalization of request", e);
+		}
+	}
+
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		git.closeScope();
+		onClose();
 	}
 
 	@Override
 	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		git.closeScope();
+		onClose();
 	}
 }
