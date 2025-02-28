78237c5b011e0420b095c1943d95a073dffdfdc4
TheEpicBlock <git.teb@theepicblock.nl>
2025-02-28 15:58:55 +0100

    Cleanup

--- platform_api/src/main/java/net/modfest/platform/misc/ThreadLocalHack.java
+++ platform_api/src/main/java/net/modfest/platform/misc/ThreadLocalHack.java
@@ -9,22 +9,6 @@ public class ThreadLocalHack {
 			Method m = ThreadLocal.class.getDeclaredMethod("get", Thread.class);
 			m.setAccessible(true);
 			return (T)m.invoke(threadLocal, thread);
-//			// https://stackoverflow.com/questions/5180114/threadlocal-value-access-across-different-threads
-//			Field field = Thread.class.getDeclaredField("threadLocals");
-//			field.setAccessible(true);
-//			Object map = field.get(thread);
-//
-//			Method method = null;
-//			method = Class.forName("java.lang.ThreadLocal$ThreadLocalMap").getDeclaredMethod("getEntry", ThreadLocal.class);
-//
-//			method.setAccessible(true);
-//			WeakReference<?> entry = (WeakReference<?>)method.invoke(map, threadLocal);
-//
-//			Field valueField = Class.forName("java.lang.ThreadLocal$ThreadLocalMap$Entry").getDeclaredField("value");
-//			valueField.setAccessible(true);
-//			Object value = valueField.get(entry);
-//
-//			return (T)value;
 		} catch (NoSuchMethodException | InvocationTargetException |
 				 IllegalAccessException e) {
 			throw new RuntimeException(e);
