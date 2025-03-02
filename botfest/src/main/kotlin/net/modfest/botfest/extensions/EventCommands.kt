50e5d47bd8a3302bcafe2ed961f52ac4d1a684e0
sisby-folk <nora.herrah.heartstrings@gmail.com>
2025-03-02 18:34:37 +1100

    un-nest unregister command

--- botfest/src/main/kotlin/net/modfest/botfest/extensions/EventCommands.kt
+++ botfest/src/main/kotlin/net/modfest/botfest/extensions/EventCommands.kt
@@ -1,21 +1,13 @@
 package net.modfest.botfest.extensions
 
 import dev.kord.common.annotation.KordUnsafe
-import dev.kord.core.behavior.interaction.ActionInteractionBehavior
 import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.core.behavior.interaction.response.EphemeralMessageInteractionResponseBehavior
 import dev.kord.core.behavior.interaction.response.edit
 import dev.kord.core.entity.User
-import dev.kord.core.event.interaction.ButtonInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildButtonInteractionCreateEvent
-import dev.kord.core.event.interaction.InteractionCreateEvent
 import dev.kord.rest.builder.message.create.InteractionResponseCreateBuilder
-import dev.kordex.core.commands.Arguments
-import dev.kordex.core.commands.application.slash.EphemeralSlashCommand
-import dev.kordex.core.commands.application.slash.ephemeralSubCommand
-import dev.kordex.core.commands.application.slash.group
 import dev.kordex.core.components.forms.ModalForm
-import dev.kordex.core.events.EventContext
 import dev.kordex.core.extensions.Extension
 import dev.kordex.core.extensions.ephemeralSlashCommand
 import dev.kordex.core.extensions.event
@@ -26,18 +18,14 @@ import dev.kordex.core.koin.KordExKoinComponent
 import dev.kordex.core.types.TranslatableContext
 import dev.kordex.modules.dev.unsafe.annotations.UnsafeAPI
 import dev.kordex.modules.dev.unsafe.commands.slash.InitialSlashCommandResponse
-import dev.kordex.modules.dev.unsafe.commands.slash.UnsafeSlashCommand
-import dev.kordex.modules.dev.unsafe.components.forms.UnsafeModalForm
 import dev.kordex.modules.dev.unsafe.extensions.unsafeSlashCommand
 import net.modfest.botfest.CommandReferences
 import net.modfest.botfest.MAIN_GUILD_ID
 import net.modfest.botfest.Platform
 import net.modfest.botfest.i18n.Translations
-import net.modfest.platform.pojo.SubmitRequestOther
 import net.modfest.platform.pojo.UserCreateData
 import org.koin.core.component.inject
 import java.util.*
-import java.util.regex.Pattern
 
 /**
  * Provides various debugging commands
@@ -86,58 +74,51 @@ class EventCommands : Extension(), KordExKoinComponent {
 			}
 		}
 
-		// event subcommands
 		ephemeralSlashCommand {
-			name = Translations.Commands.Group.Event.name
-			description = Translations.Commands.Group.Event.description
+			name = Translations.Commands.Event.Unregister.name
+			description = Translations.Commands.Event.Unregister.description
 
 			guild(MAIN_GUILD_ID)
 
-			// unregister command
-			cmds.unregisterCommand = ephemeralSubCommand {
-				name = Translations.Commands.Event.Unregister.name
-				description = Translations.Commands.Event.Unregister.description
+			action {
+				var curEvent = platform.getCurrentEvent().event
 
-				action {
-					var curEvent = platform.getCurrentEvent().event;
-
-					if (curEvent == null) {
-						respond {
-							content = Translations.Commands.Event.Unregister.Response.unavailable
-								.withContext(this@action)
-								.translateNamed()
-						}
-						return@action
-					}
-
-					val eventData = platform.getEvent(curEvent)
-					val platformUser = platform.getUser(user)
-					if (platformUser == null || !platformUser.registered.contains(curEvent)) {
-						respond {
-							content = Translations.Commands.Event.Unregister.Response.none
-								.withContext(this@action)
-								.translateNamed(
-									"event" to eventData.name
-								)
-						}
-						return@action
-					}
-
-					platform.withAuth(user).unregisterMe(eventData)
+				if (curEvent == null) {
 					respond {
-						content = Translations.Commands.Event.Unregister.Response.success
+						content = Translations.Commands.Event.Unregister.Response.unavailable
+							.withContext(this@action)
+							.translateNamed()
+					}
+					return@action
+				}
+
+				val eventData = platform.getEvent(curEvent)
+				val platformUser = platform.getUser(user)
+				if (platformUser == null || !platformUser.registered.contains(curEvent)) {
+					respond {
+						content = Translations.Commands.Event.Unregister.Response.none
 							.withContext(this@action)
 							.translateNamed(
 								"event" to eventData.name
 							)
 					}
+					return@action
+				}
+
+				platform.withAuth(user).unregisterMe(eventData)
+				respond {
+					content = Translations.Commands.Event.Unregister.Response.success
+						.withContext(this@action)
+						.translateNamed(
+							"event" to eventData.name
+						)
 				}
 			}
 		}
 	}
 
 	suspend inline fun TranslatableContext.doRegister(respond: (InteractionResponseCreateBuilder.() -> Unit) -> EphemeralMessageInteractionResponseBehavior, ackBlank: () -> EphemeralMessageInteractionResponseBehavior, sendModal: (ModalForm) -> EphemeralMessageInteractionResponseBehavior?, user: User) {
-		val curEvent = platform.getCurrentEvent().event;
+		val curEvent = platform.getCurrentEvent().event
 		if (curEvent == null) {
 			val c = Translations.Commands.Register.Response.noevent
 				.withContext(this@doRegister)
