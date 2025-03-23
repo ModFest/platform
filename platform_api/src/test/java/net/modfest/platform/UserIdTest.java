44c1d2cc71351edd34f5767d42b1325f803bcd4d
TheEpicBlock <git.teb@theepicblock.nl>
2025-03-24 00:30:33 +0100

    [platform] fix UserIdTest taking too long
    Having the test be repeated inside junit produced very funny results. But seeing as it's genuinely taking a few seconds it seems worth it to yeet it.

--- platform_api/src/test/java/net/modfest/platform/UserIdTest.java
+++ platform_api/src/test/java/net/modfest/platform/UserIdTest.java
@@ -2,16 +2,18 @@ package net.modfest.platform;
 
 import net.modfest.platform.misc.MfUserId;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 public class UserIdTest {
-	@RepeatedTest(10000)
+	@Test
 	public void testRandom() {
-		var randomId = MfUserId.generateRandom();
+		for (int i = 0; i < 2000; i++) {
+			var randomId = MfUserId.generateRandom();
 
-		// User ids should have a length of 5
-		Assertions.assertEquals(5, randomId.length(), randomId + " has wrong length");
-		// User ids cannot start with a zero. This is to avoid errors if you convert them to an integer and back
-		Assertions.assertNotEquals('0', randomId.charAt(0), randomId + " is invalid");
+			// User ids should have a length of 5
+			Assertions.assertEquals(5, randomId.length(), randomId + " has wrong length");
+			// User ids cannot start with a zero. This is to avoid errors if you convert them to an integer and back
+			Assertions.assertNotEquals('0', randomId.charAt(0), randomId + " is invalid");
+		}
 	}
 }
